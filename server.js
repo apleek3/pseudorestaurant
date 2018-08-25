196171b735cd0e48ba396213bd20fa27dcadbec6
apleek3 <apleek3@gmail.com>
2018-08-25 11:38:43 -0700

    new commit

--- server.js
+++ server.js
@@ -13,6 +13,32 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+// Open Tables
+var characters = [
+    {
+      routeName: "yoda",
+      name: "Yoda",
+      role: "Jedi Master",
+      age: 900,
+      forcePoints: 2000
+    },
+    {
+      routeName: "darthmaul",
+      name: "Darth Maul",
+      role: "Sith Lord",
+      age: 200,
+      forcePoints: 1200
+    },
+    {
+      routeName: "obiwankenobi",
+      name: "Obi Wan Kenobi",
+      role: "Jedi Master",
+      age: 55,
+      forcePoints: 1350
+    }
+  ];
+
 // Routes
 // =============================================================
 
